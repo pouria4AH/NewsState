8cc2cedbe8ef8a3f8fbfa0d7e1c247a7521ef6e2
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-05-15 18:38:49 +0430

    init basic dataLayer

---
+++ NewsState.DataLayer/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace NewsState.DataLayer.Entities
+{
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
---
+++ NewsState.DataLayer/NewsStateDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewsState.DataLayer
+{
+    public class NewsStateDbContext : DbContext
+    {
+        public NewsStateDbContext(DbContextOptions<NewsStateDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
