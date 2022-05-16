44c4032965aaecd69e292b90eeb8423015727284
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-05-16 18:07:00 +0430

    add admin panel

---
+++ NewsState.Web/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsState.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin")]
+    public class BaseController : Controller
+    {
+        protected string ErrorMessage = "ErrorMessage";
+        protected string InfoMessage = "InfoMessage";
+        protected string SuccessMessage = "SuccessMessage";
+        protected string WarningMessage = "WarningMessage";
+    }
+}
---
+++ NewsState.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsState.Web.Areas.Admin.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View("AdminIndex");
+        }
+    }
+}
