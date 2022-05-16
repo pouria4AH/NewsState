9d612a8c6e354832eea1229dee2865018e3a6564
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-05-17 00:08:11 +0430

    add main team

---
+++ NewsState.Web/wwwroot/js/MyWeb.js
@@ -0,0 +1,40 @@
+// Grab elements
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+   // throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+};
+
+//Nav styles on scroll
+const scrollHeader = () =>{
+    const navbarElement = selectElement('#header');
+    if(this.scrollY >= 15) {
+        navbarElement.classList.add('activated');
+    } else {
+        navbarElement.classList.remove('activated');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchContainer = selectElement('#search-form-container');
+
+const toggleMenu = () =>{
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+// Open/Close search form popup
+formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
+// -- Close the search form popup on ESC keypress
+window.addEventListener('keyup', (event) => {
+    if(event.key === 'Escape') searchContainer.classList.remove('activated');
+});
